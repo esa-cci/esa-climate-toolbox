f2feb89c234c658394d886fc8cf93c04ef370ccd
Tonio Fincke <tonio.fincke@brockmann-consult.de>
2024-02-26 19:42:51 +0100

    adapted test

--- test/ops/test_arithmetics.py
+++ test/ops/test_arithmetics.py
@@ -10,6 +10,7 @@ import xarray as xr
 
 from esa_climate_toolbox.core.op import OP_REGISTRY
 from esa_climate_toolbox.ops import arithmetics
+from esa_climate_toolbox.ops import diff
 from esa_climate_toolbox.util.misc import object_to_qualified_name
 
 
@@ -38,28 +39,26 @@ class TestDsArithmetics(TestCase):
             'lat': np.linspace(-88, 88, 45),
             'lon': np.linspace(-178, 178, 90)})
 
-        actual = arithmetics.arithmetics(dataset, '+2, -2, *3, /3, *4')
+        actual = arithmetics(dataset, '+2, -2, *3, /3, *4')
         assert_dataset_equal(expected * 4, actual)
 
-        actual = arithmetics.arithmetics(
+        actual = arithmetics(
             dataset, 'exp, log, *10, log10, *2, log2, +1.5'
         )
         assert_dataset_equal(expected * 2.5, actual)
 
-        actual = arithmetics.arithmetics(dataset, 'exp, -1, log1p, +3')
+        actual = arithmetics(dataset, 'exp, -1, log1p, +3')
         assert_dataset_equal(expected * 4, actual)
 
         with self.assertRaises(ValueError) as err:
-            arithmetics.arithmetics(dataset, 'not')
+            arithmetics(dataset, 'not')
         self.assertTrue('not implemented' in str(err.exception))
 
     def test_registered(self):
         """
         Test the operation when invoked through the OP_REGISTRY
         """
-        reg_op = OP_REGISTRY.get_op(object_to_qualified_name(
-            arithmetics.arithmetics)
-        )
+        reg_op = OP_REGISTRY.get_op(object_to_qualified_name(arithmetics))
         dataset = xr.Dataset({
             'first': (['lat', 'lon', 'time'], np.ones([45, 90, 3])),
             'second': (['lat', 'lon', 'time'], np.ones([45, 90, 3])),
@@ -72,7 +71,7 @@ class TestDsArithmetics(TestCase):
             'lat': np.linspace(-88, 88, 45),
             'lon': np.linspace(-178, 178, 90)})
 
-        actual = reg_op(ds=dataset, op='+2, -2, *3, /3, *4')
+        actual = reg_op(ds=dataset, ops='+2, -2, *3, /3, *4')
         assert_dataset_equal(expected * 4, actual)
 
 
@@ -94,7 +93,7 @@ class TestDiff(TestCase):
             'second': (['lat', 'lon', 'time'], np.ones([45, 90, 3])),
             'lat': np.linspace(-88, 88, 45),
             'lon': np.linspace(-178, 178, 90)})
-        actual = arithmetics.diff(dataset, dataset * 2)
+        actual = diff(dataset, dataset * 2)
         assert_dataset_equal(expected * -1, actual)
 
         # Test variable mismatch
@@ -115,9 +114,9 @@ class TestDiff(TestCase):
             'lat': np.linspace(-88, 88, 45),
             'lon': np.linspace(-178, 178, 90)})
 
-        actual = arithmetics.diff(ds, ds1)
+        actual = diff(ds, ds1)
         assert_dataset_equal(expected, actual)
-        actual = arithmetics.diff(ds1, ds)
+        actual = diff(ds1, ds)
         assert_dataset_equal(expected, actual)
 
         # Test date range mismatch
@@ -141,10 +140,10 @@ class TestDiff(TestCase):
             'lat': np.linspace(-88, 88, 45),
             'lon': np.linspace(-178, 178, 90)})
 
-        actual = arithmetics.diff(ds, ds1)
+        actual = diff(ds, ds1)
         assert_dataset_equal(actual, expected)
 
-        actual = arithmetics.diff(ds, ds1.drop_vars(['time', ]))
+        actual = diff(ds, ds1.drop_vars(['time', ]))
         expected['time'] = [datetime(2000, x, 1) for x in range(1, 13)]
         assert_dataset_equal(actual, expected)
 
@@ -166,12 +165,12 @@ class TestDiff(TestCase):
             'second': (['lat', 'lon', 'time'], np.zeros([45, 90, 3])),
             'lat': np.linspace(-88, 88, 45),
             'lon': np.linspace(-178, 178, 90)})
-        actual = arithmetics.diff(ds, ds1)
+        actual = diff(ds, ds1)
         assert_dataset_equal(expected, actual)
 
         ds['time'] = [datetime(2000, x, 1) for x in range(1, 4)]
         expected['time'] = [datetime(2000, x, 1) for x in range(1, 4)]
-        actual = arithmetics.diff(ds, ds1)
+        actual = diff(ds, ds1)
         assert_dataset_equal(expected, actual)
 
         ds1 = xr.Dataset({
@@ -180,19 +179,19 @@ class TestDiff(TestCase):
             'lat': np.linspace(-88, 88, 45),
             'lon': np.linspace(-178, 178, 90),
             'time': [datetime(2001, 1, 1)]})
-        actual = arithmetics.diff(ds, ds1)
+        actual = diff(ds, ds1)
         assert_dataset_equal(expected, actual)
 
         ds1 = ds1.squeeze('time')
         ds1['time'] = 1
-        actual = arithmetics.diff(ds, ds1)
+        actual = diff(ds, ds1)
         assert_dataset_equal(expected, actual)
 
     def test_registered(self):
         """
         Test the operation when invoked from the OP_REGISTRY
         """
-        reg_op = OP_REGISTRY.get_op(object_to_qualified_name(arithmetics.diff))
+        reg_op = OP_REGISTRY.get_op(object_to_qualified_name(diff))
         dataset = xr.Dataset({
             'first': (['lat', 'lon', 'time'], np.ones([45, 90, 3])),
             'second': (['lat', 'lon', 'time'], np.ones([45, 90, 3])),
