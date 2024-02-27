2055eb4b219ba284c8ebd716b059e05f240c8d08
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2024-02-27 19:21:32 +0100

    Added tests

--- esa_climate_toolbox/ds/kcaccess.py
+++ esa_climate_toolbox/ds/kcaccess.py
@@ -28,12 +28,14 @@ ReferenceDataStore = get_data_store_class('reference')
 
 
 class CciKerchunkDataStore(ReferenceDataStore):
+    _kc_refs = None
 
     def __init__(self):
-        kc_refs_path = os.path.join(
-            os.path.dirname(os.path.abspath(__file__)),
-            'data/kc_refs.json'
-        )
-        with open(kc_refs_path, 'r') as fp:
-            kc_refs = json.load(fp)
-        super().__init__(kc_refs, target_options=dict(compression=None))
+        if self._kc_refs is None:
+            kc_refs_path = os.path.join(
+                os.path.dirname(os.path.abspath(__file__)),
+                'data/kc_refs.json'
+            )
+            with open(kc_refs_path, 'r') as fp:
+                self._kc_refs = json.load(fp)
+        super().__init__(self._kc_refs, target_options=dict(compression=None))
