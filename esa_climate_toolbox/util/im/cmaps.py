200d28539c57de31a6122fca80297307d6c8fadc
Tonio Fincke <tonio.fincke@brockmann-consult.de>
2024-05-19 17:15:52 +0200

    added animate_map op

---
+++ esa_climate_toolbox/util/im/cmaps.py
@@ -0,0 +1,189 @@
+# The MIT License (MIT)
+# Copyright (c) 2024 ESA Climate Change Initiative
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+import base64
+import io
+import logging
+from threading import Lock
+from typing import List
+from typing import Optional
+from typing import Sequence
+from typing import Tuple
+
+import matplotlib
+import matplotlib.colors
+import numpy as np
+from PIL import Image
+
+from .cmap_lc import LAND_COVER_CCI_CMAP
+from .cmap_lc import register_lc_color_map
+
+
+_LOG = logging.getLogger('ect')
+
+CMapTemplateEntry = Tuple[str, str, Sequence[str]]
+CMapEntry = Tuple[str, str, Sequence[Tuple[str, str]]]
+
+# Have colormaps separated into categories:
+# (taken from http://matplotlib.org/examples/color/colormaps_reference.html)
+
+_CMAP_TEMPLATES: Sequence[CMapTemplateEntry] = (
+    ('Perceptually Uniform Sequential',
+     'For many applications, a perceptually uniform colormap is the best choice - '
+     'one in which equal steps in data are perceived as equal steps in the color space',
+     ('viridis', 'inferno', 'plasma', 'magma')),
+    ('Sequential 1',
+     'These colormaps are approximately monochromatic colormaps varying smoothly '
+     'between two color tones - usually from low saturation (e.g. white) to high '
+     'saturation (e.g. a bright blue). Sequential colormaps are ideal for '
+     'representing most scientific data since they show a clear progression from '
+     'low-to-high values.',
+     ('Blues', 'BuGn', 'BuPu',
+      'GnBu', 'Greens', 'Greys', 'Oranges', 'OrRd',
+      'PuBu', 'PuBuGn', 'PuRd', 'Purples', 'RdPu',
+      'Reds', 'YlGn', 'YlGnBu', 'YlOrBr', 'YlOrRd')),
+    ('Sequential 2',
+     'Many of the values from the Sequential 2 plots are monotonically increasing.',
+     ('afmhot', 'autumn', 'bone', 'cool',
+      'copper', 'gist_heat', 'gray', 'hot',
+      'pink', 'spring', 'summer', 'winter')),
+    ('Diverging',
+     'These colormaps have a median value (usually light in color) and vary '
+     'smoothly to two different color tones at high and low values. Diverging '
+     'colormaps are ideal when your data has a median value that is significant '
+     '(e.g.  0, such that positive and negative values are represented by '
+     'different colors of the colormap).',
+     ('BrBG', 'bwr', 'coolwarm', 'PiYG', 'PRGn', 'PuOr',
+      'RdBu', 'RdGy', 'RdYlBu', 'RdYlGn', 'Spectral',
+      'seismic')),
+    ('Qualitative',
+     'These colormaps vary rapidly in color. Qualitative colormaps are useful for '
+     'choosing a set of discrete colors.',
+     ('Accent', 'Dark2', 'Paired', 'Pastel1',
+      'Pastel2', 'Set1', 'Set2', 'Set3')),
+    ('Miscellaneous',
+     'Colormaps that don\'t fit into the categories above.',
+     (LAND_COVER_CCI_CMAP, 'gist_earth', 'terrain', 'ocean', 'gist_stern',
+      'brg', 'CMRmap', 'cubehelix',
+      'gnuplot', 'gnuplot2', 'gist_ncar',
+      'nipy_spectral', 'jet', 'rainbow',
+      'gist_rainbow', 'hsv', 'flag', 'prism'))
+)
+
+_CMAPS: Optional[Sequence[CMapEntry]] = None
+
+_LOCK = Lock()
+
+
+def get_cmaps():
+    """
+    Return a JSON-serializable tuple containing records of the form:
+        (<cmap-category>, <cmap-category-description>, <cmap-tuples>),
+    where <cmap-tuples> is a tuple containing records
+    of the form (<cmap-name>, <cbar-png-bytes>), and where
+    <cbar-png-bytes> are encoded PNG images of size 256 x 2 pixels,
+    :return: all known matplotlib color maps
+    """
+
+    global _CMAPS
+    ensure_cmaps_loaded()
+    return _CMAPS
+
+
+def ensure_cmaps_loaded():
+    """
+    Loads all color maps from matplotlib and registers additional ones,
+    if not done before.
+    """
+    global _CMAP_TEMPLATES, _CMAPS
+    if _CMAPS is None:
+        _LOCK.acquire()
+        if _CMAPS is None:
+            register_lc_color_map()
+            new_cmaps: List[CMapEntry] = []
+            for cmap_category, cmap_description, cmap_names in _CMAP_TEMPLATES:
+                cbar_list = []
+                for cmap_name in cmap_names:
+                    # noinspection PyBroadException
+                    try:
+                        cmap = matplotlib.colormaps.get_cmap(cmap_name)
+                    except Exception:
+                        _LOG.warning('detected invalid colormap "%s"' % cmap_name)
+                        continue
+
+                    # Add extra colormaps with alpha gradient
+                    # see http://matplotlib.org/api/colors_api.html
+                    if type(cmap) == matplotlib.colors.LinearSegmentedColormap:
+                        new_name = cmap.name + '_alpha'
+                        # noinspection PyProtectedMember
+                        new_segmentdata = dict(cmap._segmentdata)
+                        # let alpha increase from 0.0 to 0.5
+                        new_segmentdata['alpha'] = ((0.0, 0.0, 0.0),
+                                                    (0.5, 1.0, 1.0),
+                                                    (1.0, 1.0, 1.0))
+                        new_cmap = matplotlib.colors.LinearSegmentedColormap(
+                            new_name, new_segmentdata
+                        )
+                        matplotlib.colormaps.register(cmap=new_cmap)
+                    elif type(cmap) == matplotlib.colors.ListedColormap:
+                        new_name = cmap.name + '_alpha'
+                        new_colors: List[Sequence[float]] = list(cmap.colors)
+                        a_slope = 2.0 / cmap.N
+                        a = 0
+                        for i in range(len(new_colors)):
+                            new_color = new_colors[i]
+                            if not isinstance(new_color, str):
+                                if len(new_color) == 3:
+                                    # noinspection PyTupleAssignmentBalance
+                                    r, g, b = new_color
+                                    new_colors[i] = r, g, b, a
+                                elif len(new_color) == 4:
+                                    r, g, b, a_old = new_color
+                                    new_colors[i] = r, g, b, min(a, a_old)
+                            a += a_slope
+                            if a > 1.0:
+                                a = 1.0
+                        new_cmap = matplotlib.colors.ListedColormap(new_colors, name=new_name)
+                        matplotlib.colormaps.register(cmap=new_cmap)
+                    else:
+                        new_name = cmap.name + '_alpha' \
+                            if hasattr(cmap, 'name') else 'unknown'
+                        _LOG.warning('could not create colormap "{}" '
+                                     'because "{}" is of unknown type {}'
+                                     .format(new_name, cmap.name, type(cmap)))
+
+                    gradient = np.linspace(0, 1, 256)
+                    gradient = np.vstack((gradient, gradient))
+                    image_data = cmap(gradient, bytes=True)
+                    image = Image.fromarray(image_data, 'RGBA')
+
+                    ostream = io.BytesIO()
+                    image.save(ostream, format='PNG')
+                    cbar_png_bytes = ostream.getvalue()
+                    ostream.close()
+
+                    cbar_png_data = base64.b64encode(cbar_png_bytes)
+                    cbar_png_bytes = cbar_png_data.decode('unicode_escape')
+
+                    cbar_list.append((cmap_name, cbar_png_bytes))
+                new_cmaps.append((cmap_category, cmap_description, tuple(cbar_list)))
+            _CMAPS = tuple(new_cmaps)
+        _LOCK.release()
---
+++ test/ops/test_plot_helpers.py
@@ -0,0 +1,231 @@
+"""
+Tests for plot and animation helper functions
+"""
+
+from unittest import TestCase
+
+import xarray as xr
+import numpy as np
+import pandas as pd
+
+from esa_climate_toolbox.ops.plot_helpers import check_bounding_box
+from esa_climate_toolbox.ops.plot_helpers import determine_cmap_params
+from esa_climate_toolbox.ops.plot_helpers import get_var_data
+from esa_climate_toolbox.ops.plot_helpers import get_vars_data
+from esa_climate_toolbox.ops.plot_helpers import in_notebook
+from esa_climate_toolbox.core.types import ValidationError
+
+
+class TestCheckBoundingBox(TestCase):
+    """
+    Test check_bounding_box()
+    """
+
+    def test_nominal(self):
+        """
+        Nominal test
+        """
+        self.assertTrue(check_bounding_box(10, 20, 10, 20))
+        self.assertFalse(check_bounding_box(20, 10, 10, 20))
+        self.assertFalse(check_bounding_box(10, 20, 20, 10))
+        self.assertFalse(check_bounding_box(-91, 10, 10, 20))
+        self.assertFalse(check_bounding_box(20, 91, 10, 20))
+        self.assertFalse(check_bounding_box(10, 20, -181, 20))
+        self.assertFalse(check_bounding_box(10, 20, 10, 181))
+
+
+class TestInNotebook(TestCase):
+    """
+    Test in_notebook()
+    """
+
+    def test_nominal(self):
+        """
+        Nominal test
+        """
+        self.assertFalse(in_notebook())
+
+
+class TestGetVarData(TestCase):
+    """
+    Test get_var_data()
+    """
+
+    def test_nominal(self):
+        """
+        Nominal test
+        """
+        dataset = xr.Dataset({
+            'first': (['lat', 'lon', 'time'], np.random.rand(5, 10, 2)),
+            'second': (['lat', 'lon', 'time'], np.random.rand(5, 10, 2)),
+            'lat': np.linspace(-89.5, 89.5, 5),
+            'lon': np.linspace(-179.5, 179.5, 10),
+            'time': pd.date_range('2000-01-01', periods=2)})
+
+        indexers = {'time': '2000-01-01', 'lat': -88}
+        out = get_var_data(dataset.second, indexers, remaining_dims=['lon', ])
+        self.assertEqual(out.time, np.datetime64('2000-01-01'))
+        self.assertEqual(out.lat, -89.5)
+        self.assertEqual(len(out.lon.values), 10)
+
+        # test on data with more dims
+        dataset2 = xr.Dataset({
+            'first': (['lat', 'lon', 'layers', 'time'], np.random.rand(5, 10, 16, 2)),
+            'second': (['lat', 'lon', 'layers', 'time'], np.random.rand(5, 10, 16, 2)),
+            'lat': np.linspace(-89.5, 89.5, 5),
+            'lon': np.linspace(-179.5, 179.5, 10),
+            'layers': np.linspace(1, 16, 16),
+            'time': pd.date_range('2000-01-01', periods=2)})
+
+        indexers = {'lat': 0, 'lon': -179.5, 'layers': 5}
+        out = get_var_data(dataset2.second, indexers, remaining_dims=['time', ])
+        self.assertEqual(out.lat, 0)
+        self.assertEqual(out.lon, -179.5)
+        self.assertEqual(out.layers, 5)
+        self.assertEqual(len(out.time.values), 2)
+
+        # test when a dimension is neither in indexers nor remaining_dims specified,
+        # it will select the first element of that dimension
+        # (if remaining_dims is specified)
+        indexers = {'lat': 0, 'lon': -179.5}
+        out = get_var_data(dataset2.second, indexers, remaining_dims=['time', ])
+        self.assertEqual(out.lat, 0)
+        self.assertEqual(out.lon, -179.5)
+        self.assertEqual(out.layers, 1)
+        self.assertEqual(len(out.time.values), 2)
+
+        # when remaining_dims is not specified,
+        # it should only index based on the given indexers
+        indexers = {'lat': 0, 'lon': -179.5}
+        out = get_var_data(dataset2.second, indexers)
+        self.assertEqual(out.lat, 0)
+        self.assertEqual(out.lon, -179.5)
+        self.assertEqual(len(out.layers), 16)
+        self.assertEqual(len(out.time.values), 2)
+
+        # should raise a ValidationError when
+        # the specified dimension is not part of variable dimension names
+        indexers = {'lat': 0, 'lon': -179.5, 'time': '2000-01-01'}
+        with self.assertRaises(ValidationError) as cm:
+            get_var_data(dataset2.second, indexers, remaining_dims=['dummy', ])
+        self.assertEqual(str(cm.exception),
+                         "The specified dataset does not have a "
+                         "dimension called \'dummy\'.")
+
+        # should raise a ValidationError when a dimension is specified
+        # as both indexers and remaining_dims
+        indexers = {'lat': 0, 'lon': -179.5, 'time': '2000-01-01'}
+        with self.assertRaises(ValidationError) as cm:
+            get_var_data(dataset2.second, indexers, remaining_dims=['time', ])
+        self.assertEqual(str(cm.exception),
+                         "Dimension 'time' is also specified as indexers. "
+                         "Please ensure that a dimension is used exclusively "
+                         "either as indexers or as the selected dimension.")
+
+        # should raise a ValidationError when a dimension is specified
+        # as indexers does not exist in the given variable
+        indexers = {'lat': 0, 'lon': -179.5, 'dummy': 1}
+        with self.assertRaises(ValidationError) as cm:
+            get_var_data(dataset2.second, indexers, remaining_dims=['time', ])
+        self.assertEqual(str(cm.exception),
+                         "The specified dataset does not have a dimension "
+                         "called 'dummy'.")
+
+    def test_get_vars_data(self):
+        dataset = xr.Dataset({
+            'first': (['lat', 'lon', 'layers1', 'time'], np.random.rand(5, 10, 16, 2)),
+            'second': (['lat', 'lon', 'layers2', 'time'], np.random.rand(5, 10, 4, 2)),
+            'third': (['lat', 'lon', 'layers1', 'layers2', 'time'],
+                      np.random.rand(5, 10, 16, 4, 2)),
+            'fourth': (['lat', 'lon', 'time'], np.random.rand(5, 10, 2)),
+            'lat': np.linspace(-89.5, 89.5, 5),
+            'lon': np.linspace(-179.5, 179.5, 10),
+            'layers1': np.linspace(1, 16, 16),
+            'layers2': np.linspace(1, 4, 4),
+            'time': pd.date_range('2000-01-01', periods=2)})
+
+        indexers = {'lat': -89.5, 'lon': -179.5, 'layers1': 5, 'layers2': 4}
+        vars_data = get_vars_data(dataset, indexers, remaining_dims=['time', ])
+        self.assertEqual(9, len(vars_data.variables))
+        self.assertEqual(5, len(vars_data.dims))
+        self.assertEqual(1, len(vars_data.first.dims))
+        self.assertEqual(1, len(vars_data.second.dims))
+        self.assertEqual(1, len(vars_data.third.dims))
+        self.assertEqual(1, len(vars_data.fourth.dims))
+        # TODO (hp): is it possible to check if the right data has been selected?
+        #  When merged back to ds, the dimensions selected by the indexers
+        #  are not visible anymore.
+        self.assertEqual(2, len(vars_data.time))
+
+        indexers = {'lat': -89.5, 'lon': 179.5}
+        vars_data = get_vars_data(dataset, indexers, remaining_dims=['time', ])
+        self.assertEqual(9, len(vars_data.variables))
+        self.assertEqual(5, len(vars_data.dims))
+        self.assertEqual(1, len(vars_data.first.dims))
+        self.assertEqual(1, len(vars_data.second.dims))
+        self.assertEqual(1, len(vars_data.third.dims))
+        self.assertEqual(1, len(vars_data.fourth.dims))
+        self.assertEqual(2, len(vars_data.time))
+
+        indexers = {'lat': -89.5, 'lon': 179.5}
+        vars_data = get_vars_data(dataset, indexers)
+        self.assertEqual(9, len(vars_data.variables))
+        self.assertEqual(5, len(vars_data.dims))
+        self.assertEqual(2, len(vars_data.first.dims))
+        self.assertEqual(2, len(vars_data.second.dims))
+        self.assertEqual(3, len(vars_data.third.dims))
+        self.assertEqual(1, len(vars_data.fourth.dims))
+        self.assertEqual(2, len(vars_data.time))
+
+        # should raise a ValidationError when the specified dimension
+        # is not part of variable dimension names
+        indexers = {'lat': 0, 'lon': -179.5, 'time': '2000-01-01'}
+        with self.assertRaises(ValidationError) as cm:
+            get_vars_data(dataset, indexers, remaining_dims=['dummy', ])
+        self.assertEqual("The specified dataset does not have a dimension "
+                         "called 'dummy'.", str(cm.exception))
+
+        # should raise a ValidationError when a dimension is specified
+        # as both indexers and remaining_dims
+        indexers = {'lat': 0, 'lon': -179.5, 'time': '2000-01-01'}
+        with self.assertRaises(ValidationError) as cm:
+            get_vars_data(dataset, indexers, remaining_dims=['time', ])
+        self.assertEqual("Dimension 'time' is also specified as indexers. "
+                         "Please ensure that a dimension is used exclusively either "
+                         "as indexers or as the selected dimension.", str(cm.exception))
+
+        # should raise a ValidationError when a dimension is specified
+        # as indexers does not exist in the given variable
+        indexers = {'lat': 0, 'lon': -179.5, 'dummy': 1}
+        with self.assertRaises(ValidationError) as cm:
+            get_vars_data(dataset, indexers, remaining_dims=['time', ])
+        self.assertEqual("There are dimensions specified in indexers "
+                         "but do not match dimensions in any variables: ['dummy']",
+                         str(cm.exception))
+
+
+class TestDetermineCmapParams(TestCase):
+    """
+    Test determine_cmap_params()
+    """
+
+    def test_nominal(self):
+        """
+        Nominal test
+        """
+        # Test nominal
+        cparams = determine_cmap_params(0, 100)
+        self.assertEqual(cparams['cmap'], 'viridis')
+
+        # Test divergent
+        cparams = determine_cmap_params(-100, 100)
+        self.assertEqual(cparams['cmap'], 'RdBu_r')
+
+        # Test extents
+        cparams = determine_cmap_params(0, 100, vmin=10, vmax=90)
+        self.assertEqual(cparams['extend'], 'both')
+
+        # Test levels
+        cparams = determine_cmap_params(0, 100, levels=3, vmin=10)
+        self.assertEqual(len(cparams['levels']), 3)
+        self.assertIsNotNone(cparams['norm'])
